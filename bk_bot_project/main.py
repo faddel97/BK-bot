6d34b50ba071107ce47799e72eda4cdefd95e9cc
faddel97 <faddel.sahool@gmail.com>
2025-07-15 16:17:48 +0300

    Add files via upload

---
+++ bk_bot_project/main.py
@@ -0,0 +1,31 @@
+
+import os
+import discord
+import random
+
+TOKEN = os.getenv("TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+video_links = [
+    "https://www.youtube.com/@bkarchive/featured",
+    "https://www.youtube.com/@BasimKarbalaei",
+    "https://www.youtube.com/channel/UCH1nJIlCPQ7QAIItVrpr7WA",
+    "https://www.youtube.com/playlist?list=PLXzJdQFBKapkgwtIad7QPs8jk1OmCsbPX",
+    "https://www.youtube.com/playlist?list=PLjtjRVhLz_nxyvsPA0_9VtV8isPA-uWdE"
+]
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"✅ Bot is running as {{client.user}}")
+
+@client.event
+async def on_message(message):
+    if message.channel.id == CHANNEL_ID and message.content.strip().lower() == "مقطع":
+        video = random.choice(video_links)
+        await message.channel.send(f"🎥 مقطع عشوائي:\n{{video}}")
+
+client.run(TOKEN)
